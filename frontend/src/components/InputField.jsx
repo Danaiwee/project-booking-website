ca9b5006155d0abcabb7571d40ae7a85c7e35b08
DanaiWee <Danai_wee@hotmail.com>
2025-03-01 21:14:47 +0700

    Added InputField component

---
+++ frontend/src/components/InputField.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const InputField = ({
+  label,
+  id,
+  name,
+  placeholder,
+  type = "text",
+  value,
+  onChange,
+}) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={id} className="text-xl font-medium text-white">
+        {label}
+      </label>
+      <input
+        className="input input-info w-full"
+        type={type}
+        placeholder={placeholder}
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default InputField;
