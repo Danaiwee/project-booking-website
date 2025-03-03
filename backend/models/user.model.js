2c4be0e0028195c5edfb5f92c888faa7f9ddf577
DanaiWee <Danai_wee@hotmail.com>
2025-03-03 20:05:43 +0700

    create server, auth route and controller,  connected to mongoDB and created user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        min: 6,
+        required: true
+    },
+
+    profileImg: {
+        type:String,
+        default: ''
+    },
+
+    bookingItems: [
+        {
+            bookingId : {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Booking"
+            }
+        }
+    ],
+}, {timestamps: true})
+
+const User = mongoose.model("User", userSchema);
+export default User;
