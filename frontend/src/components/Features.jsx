2adc8234a44d6635dcaa64beab6b50ca19a876f8
DanaiWee <Danai_wee@hotmail.com>
2025-03-08 12:41:18 +0700

    Added height in feature card

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -15,20 +15,20 @@ const Features = ({hotel}) => {
             className="w-full h-60"
           />
         </figure>
-        <div className="card-body gap-0">
+        <div className="card-body gap-0 flex flex-col items-start">
           <p className="text-xs text-gray-500">{type}</p>
-          <h2 className="card-title">
+          <h2 className="card-title h-15">
             {title}
           </h2>
 
-          <div className="w-fit flex items-center gap-3 mt-2">
+          <div className="w-fit flex items-center gap-3">
             <p className="bg-blue-900 p-1.5 rounded-md text-white text-xs ">{rating.toFixed(1)}</p>
             <p className='text-xs text-gray-500'>{rate}</p> 
           </div>
 
           <div className='w-fit flex items-center gap-3 mt-2'>
             <p className='text-gray-500 text-xs'>Starting from</p>
-            <p className='text-lg font-extrabold text-gray-900'>THB {minPrice.toLocaleString()}</p>
+            <p className='text-lg font-extrabold text-gray-900'>THB {minPrice.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
           </div>
         </div>  
       </Link>
