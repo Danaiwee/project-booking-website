059b4244f7481d9650c31d28e9433527dec847f8
DanaiWee <Danai_wee@hotmail.com>
2025-03-09 18:33:46 +0700

    Improve code in Features component

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSearchStore } from "../store/useSearchStore.js";
 
 const Features = ({hotel}) => {
   const {title, rating, type, minPrice, images } = hotel;
-
   const rate = rating<5 ? "Good" : rating<8 ? "Very good" : "Excellent"
+
+  const {setSearchDetails} = useSearchStore();
+
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await setSearchDetails();
+      navigate(`/details/${hotel?._id}`);
+
+    } catch (error) {
+      console.log("Error in Features: ", error);
+    }
+  };
   
   return (
     <div className="card bg-base-100 w-75 shadow-lg overflow-hidden">
-      <Link to={`/details/${hotel?._id}`}>
+      <button className='cursor-pointer' onClick={handleClick}>
         <figure>
           <img
             src={images[0]}
@@ -31,7 +46,7 @@ const Features = ({hotel}) => {
             <p className='text-lg font-extrabold text-gray-900'>THB {minPrice.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
           </div>
         </div>  
-      </Link>
+      </button>
     </div>
   );
 };
