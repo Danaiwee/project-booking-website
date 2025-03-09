82dc9c0b8d8dd99d135fcafc7f2f6f394bb718c4
DanaiWee <Danai_wee@hotmail.com>
2025-03-09 18:12:29 +0700

    Improve propertyTypes component

--- frontend/src/components/PropertyTypes.jsx
+++ frontend/src/components/PropertyTypes.jsx
@@ -1,27 +1,54 @@
-import hotelImg from '/propertyTypes/hotels.jpg';
-import apartmentImg from '/propertyTypes/apartments.jpg';
-import resortImg from '/propertyTypes/resorts.jpg';
-import villaImg from '/propertyTypes/villas.jpg';
-import cabinImg from '/propertyTypes/cabins.jpg';
-import PropertyType from './PropertyType';
+import hotelImg from "/propertyTypes/hotels.jpg";
+import apartmentImg from "/propertyTypes/apartments.jpg";
+import resortImg from "/propertyTypes/resorts.jpg";
+import villaImg from "/propertyTypes/villas.jpg";
+import cabinImg from "/propertyTypes/cabins.jpg";
+import PropertyType from "./PropertyType";
 
 const PropertyTypes = ({ type }) => {
   const properties = [
-    { id: 1, title: "Hotels", amount: type[0]?.count, image: hotelImg, src:type[0]?.type },
-    { id: 2, title: "Apartments", amount: type[1]?.count, image: apartmentImg, src:type[1]?.type },
-    { id: 3, title: "Resorts", amount: type[2]?.count, image: resortImg, src:type[2]?.type },
-    { id: 4, title: "Villas", amount: type[3]?.count, image: villaImg, src:type[3]?.type },
-    { id: 5, title: "House", amount: type[4]?.count, image: cabinImg, src:type[4]?.type },
+    {
+      id: 1,
+      title: "Hotels",
+      amount: type[0]?.count || 0,
+      image: hotelImg,
+      src: type[0]?.type || "hotel",
+    },
+    {
+      id: 2,
+      title: "Apartments",
+      amount: type[1]?.count || 0,
+      image: apartmentImg,
+      src: type[1]?.type || "apartment",
+    },
+    {
+      id: 3,
+      title: "Resorts",
+      amount: type[2]?.count || 0,
+      image: resortImg,
+      src: type[2]?.type || "resort",
+    },
+    {
+      id: 4,
+      title: "Villas",
+      amount: type[3]?.count || 0,
+      image: villaImg,
+      src: type[3]?.type || "villa",
+    },
+    {
+      id: 5,
+      title: "House",
+      amount: type[4]?.count || 0,
+      image: cabinImg,
+      src: type[4]?.type || "house",
+    },
   ];
 
   return (
     <>
-    {properties.map((property) => (
-        <PropertyType 
-            key={property.id}
-            property={property}
-        />
-    ) )}
+      {properties.map((property) => (
+        <PropertyType key={property.id} property={property} />
+      ))}
     </>
   );
 };
