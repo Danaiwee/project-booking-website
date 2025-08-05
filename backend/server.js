2a4518176da6387a8bfa620e306f378d4afcd35c
DanaiWee <Danai_wee@hotmail.com>
2025-08-05 19:08:28 +0700

    Fixed deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use("/api/rooms", roomRoutes);
 app.use("/api/booking", bookingRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
