cfc7815f5698b7a8ab9e06e1748e623d8a2fa0b6
DanaiWee <Danai_wee@hotmail.com>
2025-12-17 16:51:47 +0700

    Fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
@@ -30,14 +28,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/rooms", roomRoutes);
 app.use("/api/booking", bookingRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
